25fce0f80cae130e4bf04316ed41c6115adcc5ea
Max Brauer <max@transcode.de>
2016-06-01 21:44:11 +0200

    Fixed main.js.

--- ariane/static/js/main.js
+++ ariane/static/js/main.js
@@ -1,7 +1,8 @@
 $(document).ready(function () {
     var message_container = $('#messages');
     var input = $('#input');
-    var socket = new WebSocket("ws://127.0.0.1:8000/ws");
+
+    var socket = new WebSocket('ws://' + window.location.host + '/ws');
     socket.onmessage = function(e) {
         message_container.append('<p>' + e.data + '</p>');
     };
