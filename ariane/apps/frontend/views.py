745895f04385676a34b4d7fd71f8f1a2e44a82c6
Max Brauer <max@transcode.de>
2016-06-04 11:26:50 +0200

    Added docstrings to views.

--- ariane/apps/frontend/views.py
+++ ariane/apps/frontend/views.py
@@ -1,13 +1,23 @@
 from braces.views import LoginRequiredMixin
-
 from django.shortcuts import redirect
 from django.views.generic import TemplateView
 
 
 class LandingPageView(TemplateView):
+    """LandingPage for registering and logging in."""
+
     template_name = 'frontend/index.html'
 
     def get(self, request, *args, **kwargs):
+        """
+        Handle the get request.
+
+        Redirects to ariane if logged in. Otherwise, renders the landingpage.
+
+        Args:
+            request (Request): the HTTPRequest for processing.
+            *args, **kwargs: additional arguments, used for the inherited views.
+        """
         if request.user.is_authenticated():
             response = redirect('ariane')
         else:
@@ -16,4 +26,6 @@ class LandingPageView(TemplateView):
 
 
 class ArianeView(LoginRequiredMixin, TemplateView):
+    """The core view of the project."""
+
     template_name = 'frontend/ariane.html'
