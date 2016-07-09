2a1f8ea0f11db9b229d3a9f7cc5fb425a6572f87
Max Brauer <max@transcode.de>
2016-07-09 14:21:00 +0200

    Started refactoring JS.

--- ariane/static/js/main.js
+++ ariane/static/js/main.js
@@ -1,34 +1,53 @@
+var languages = {
+    "en-GB": "UK English Female",
+    "de-DE": "Deutsch Female"
+};
 
-var recognition = (function initializeRecognition() {
-    var r = new webkitSpeechRecognition();
-    r.lang = "en-GB";
-    r.continuous = true;
-    r.interimResults = false;
-    return r;
-})();
+function Ariane(lang) {
+    //this._rec = new webkitSpeechRecognition();
+    this._rec = {
+        start: function() {},
+        stop: function() {}
+    };
+    this._rec.lang = lang;
+    this.voice = languages[lang];
+    this._rec.continuous = true;
+    this._rec.interimResults = false;
+    this.listening = false;
 
-function startRecognition() {
-    recognition.start();
+    this.start_recognition = function() {
+        this.listening = true;
+        this._rec.start();
+    };
+
+    this.stop_recognition = function() {
+        this.listening = false;
+        this._rec.stop();
+    };
+
+    this.say = function(text) {
+        responsiveVoice.speak(
+            text,
+            this.voice,
+            {
+                onstart: this.stop_recognition,
+                onend: this.start_recognition
+            }
+        );
+    };
 }
 
-function stopRecognition() {
-    recognition.stop();
-}
-
-
-$(document).ready(function () {
-
-    var message_container = $('#messages');
-    var input = $('#input');
-
-    var socket = new WebSocket('ws://' + window.location.host + '/ws');
-    socket.onmessage = function(e) {
-        responsiveVoice.speak(e.data, "UK English Female", {
-            onstart: stopRecognition, onend: startRecognition});
-    };
-    recognition.onresult = function(e) {
-        message = e.results[e.results.length -1];
-        socket.send(message[0].transcript);
-    };
-    startRecognition();
-});
+//$(document).ready(function () {
+//
+//    var message_container = $('#messages');
+//    var input = $('#input');
+//
+//    var socket = new WebSocket('ws://' + window.location.host + '/ws');
+//    socket.onmessage = function(e) {
+//
+//    recognition.onresult = function(e) {
+//        message = e.results[e.results.length -1];
+//        socket.send(message[0].transcript);
+//    };
+//    startRecognition();
+//});
