e528aa7aeb8231dae3e9ee3f8191e9016e1fd294
Max Brauer <max@transcode.de>
2016-08-14 10:55:56 +0200

    Started IU development.

--- ariane/static/js/main.js
+++ ariane/static/js/main.js
@@ -4,51 +4,99 @@ var languages = {
 };
 
 function Ariane(lang) {
-    //this._rec = new webkitSpeechRecognition();
-    this._rec = {
-        start: function() {},
-        stop: function() {}
-    };
-    this._rec.lang = lang;
-    this.voice = languages[lang];
-    this._rec.continuous = true;
-    this._rec.interimResults = false;
-    this.listening = false;
+    self = this;
+    self.listening = ko.observable(false);
 
-    this.start_recognition = function() {
-        this.listening = true;
-        this._rec.start();
+    self.leftTemplate = ko.observable('empty');
+    self.leftData = ko.observable();
+    self.centerTopTemplate = ko.observable('empty');
+    self.centerTopData = ko.observable();
+    self.centerBottomTemplate = ko.observable('empty');
+    self.centerBottomData = ko.observable();
+    self.rightTemplate = ko.observable('empty');
+    self.rightData = ko.observable();
+    self.active = ko.observable(false);
+
+    self.message = ko.observable('');
+
+    self.start_recognition = function() {
+        self.listening(true);
+        self._rec.start();
     };
 
-    this.stop_recognition = function() {
-        this.listening = false;
-        this._rec.stop();
+    self.stop_recognition = function() {
+        self.listening(false);
+        self._rec.stop();
     };
 
-    this.say = function(text) {
+    self.say = function(text) {
         responsiveVoice.speak(
             text,
-            this.voice,
+            self.voice,
             {
-                onstart: this.stop_recognition,
-                onend: this.start_recognition
+                onstart: self.stop_recognition,
+                onend: self.start_recognition
             }
         );
         return true;
     };
-}
 
-//$(document).ready(function () {
-//
-//    var message_container = $('#messages');
-//    var input = $('#input');
-//
-//    var socket = new WebSocket('ws://' + window.location.host + '/ws');
-//    socket.onmessage = function(e) {
-//
-//    recognition.onresult = function(e) {
-//        message = e.results[e.results.length -1];
-//        socket.send(message[0].transcript);
-//    };
-//    startRecognition();
-//});
+    self.initialize = function() {self._initialize.knockout();};
+
+    /* istanbul ignore next:
+        * lack of coverage due to setTimeout
+        * only UI specific sugar
+        * speechRecognition & websocket will be faked in tests anyway.
+    */
+    self._initialize = {
+        knockout: function() {
+            ko.applyBindings(self);
+            self.centerTopData({'text': 'Initializing user interface...'});
+            self.centerTopTemplate('infoText');
+            setTimeout(function() {
+                $('.fourth_arc').removeClass('invis_arc');
+                setTimeout(function() {
+                    $('.fourth_arc').addClass('animated');
+                    self._initialize.voice();
+                }, initializationTimeout);
+            }, initializationTimeout);
+        },
+        voice: function() {
+            self.centerTopData({'text': 'Initializing language...'});
+            setTimeout(function() {
+                $('.third_arc').removeClass('invis_arc');
+                self.voice = languages[lang];
+                setTimeout(function() {
+                    $('.third_arc').addClass('animated');
+                    self._initialize.speechRecognition();
+                }, initializationTimeout);
+            }, initializationTimeout);
+        },
+        speechRecognition: function() {
+            self.centerTopData({'text': 'Initializing speech recognition...'});
+            setTimeout(function() {
+                $('.second_arc').removeClass('invis_arc');
+                self._rec = new webkitSpeechRecognition();
+                self._rec.lang = lang;
+                self._rec.continuous = true;
+                self._rec.interimResults = false;
+                setTimeout(function() {
+                    $('.second_arc').addClass('animated');
+                    self._initialize.websocket();
+                }, initializationTimeout);
+            }, initializationTimeout);
+        },
+        websocket: function() {
+            self.centerTopData({'text': 'Establishing connection...'});
+            setTimeout(function() {
+                $('.first_arc').removeClass('invis_arc');
+                setTimeout(function() {
+                    $('.first_arc').addClass('animated');
+                    self.active(true);
+                    self.centerTopTemplate('empty');
+                    self.centerTopData({});
+                }, initializationTimeout);
+            }, initializationTimeout);
+        }
+    };
+}
