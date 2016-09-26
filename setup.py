913584afe95518ac6f88cc0d7b037e305790bc54
requires.io <support@requires.io>
2016-09-26 12:55:23 +0200

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ requires = [
     'django-braces==1.9.0',
     'django-configurations==2.0',
     'django-crispy-forms==1.6.0',
-    'django-grappelli==2.8.1',
+    'django-grappelli==2.8.2',
     'django-model-utils==2.6',
     'envdir==0.7',
     'psycopg2==2.6.2',
