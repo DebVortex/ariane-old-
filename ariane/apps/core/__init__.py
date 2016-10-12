3613f17ec92e378836400917ef6b99040186d912
Max Brauer <max@transcode.de>
2016-10-12 20:21:47 +0200

    core: refactored Ariane class to handle wit.ai intents.

--- ariane/apps/core/__init__.py
+++ ariane/apps/core/__init__.py
@@ -1,53 +1,32 @@
-from django.conf import settings
-
-
 class Ariane(object):
     """Main class for Ariane, the voice controlled assitant."""
 
-    def __init__(self):
-        """Instantiate Ariane.
+    _brain = {}
+    js_files = []
 
-        This sets up the brain of Ariane. The brain is a dictionary, containing
-        each supported language as key. The value, is a dictionary itself and
-        will later be populated with a mapping from each supported keyword to
-        the function that should be executet.
-        Additionaly, this function also initializes an empty list for JS files
-        that have to be rendered in the frontend.
-        """
-        self._brain = {lang: {} for lang in settings.ARIANE_SUPPORTED_LANGUAGES}
-        self.js_files = []
-
-    def register(self, languages, func):
+    def register(self, intent, func):
         """Register a new function to Ariane.
 
         Args:
-            languages (dict): A dictionary, containing a supported languages as key and a list of
-                keywords that should be registered to the provided func.
+            intent (string): a string, returned by wit.ai to determine the correct function.
             func (callable): a callable that should be used, once the keyword is pressent in the
                 message. Musst accept a sting as first argument.
         """
-        for lang in languages:
-            if lang not in self._brain:
-                raise AttributeError("Language {} not supported".format(lang))
-            for keyword in languages[lang]:
-                if keyword in self._brain[lang]:
-                    raise AttributeError("Keyword {} already registered.".format(keyword))
-                self._brain[lang][keyword] = func
+        if intent in self._brain:
+            raise AttributeError("Intent {} already registered.".format(intent))
+        self._brain[intent] = func
 
 
-ariane = Ariane()
-
-
-def register(languages, func):
+def register(intent, func):
     """Register function that will be called inside the brain.py.
 
     Args:
-        languages (dict): A dictionary, containing a supported languages as key and a list of
-            keywords that should be registered to theprovided func.
+        intent (string): a string, returned by wit.ai to determine the correct function.
         func (callable): a callable that should be used, once the keyword is pressent in the
-            message. Musst accept a sting as first argument.
+            message. Musst accept a sting as first argument. Musst be able to accept a language
+            kwarg.
     """
-    ariane.register(languages, func)
+    Ariane().register(intent, func)
 
 
 def register_js(js_file, template_id=None):
@@ -61,4 +40,4 @@ def register_js(js_file, template_id=None):
         template_id (string): The id used for the script tag in html. If provided, the type will
         be set to text/html.
     """
-    ariane.js_files.append((js_file, template_id))
+    Ariane().js_files.append((js_file, template_id))
