4859b84906928c264598bbf9c2269f7d10152a93
Max Brauer <max@transcode.de>
2016-10-21 17:55:50 +0200

    core: updated ariane to work with wit.ai

--- ariane/apps/core/__init__.py
+++ ariane/apps/core/__init__.py
@@ -1,10 +1,22 @@
+from django.conf import settings
+
+from wit import Wit
+
+
 class Ariane(object):
     """Main class for Ariane, the voice controlled assitant."""
 
-    _brain = {}
     js_files = []
+    actions = {}
 
-    def register(self, intent, func):
+    def __init__(self, language):
+        self.language = language
+        self.client = Wit(
+            access_token=settings.WIT_ACCESS_TOKENS[self.language],
+        )
+
+    @classmethod
+    def register(cls, intent, func):
         """Register a new function to Ariane.
 
         Args:
@@ -12,21 +24,39 @@ class Ariane(object):
             func (callable): a callable that should be used, once the keyword is pressent in the
                 message. Musst accept a sting as first argument.
         """
-        if intent in self._brain:
-            raise AttributeError("Intent {} already registered.".format(intent))
-        self._brain[intent] = func
+        if intent in cls.actions:
+            raise AttributeError(
+                "There is already a function for the intent '{}'' registerd.".format(intent)
+            )
+        cls.actions[intent] = func
+
+    def handle(self, message):
+        resp = self.client.message(message)
+        # ToDo: Handle unknown or to bad confidence
+        return self.actions[resp['entities']['intent'][0]['value']](self, resp)
 
 
-def register(intent, func):
+def register(intent):
     """Register function that will be called inside the brain.py.
 
+    Usage:
+        @register("my_intent")
+        def my_function(cls, message):
+            [...]
+
     Args:
-        intent (string): a string, returned by wit.ai to determine the correct function.
-        func (callable): a callable that should be used, once the keyword is pressent in the
-            message. Musst accept a sting as first argument. Musst be able to accept a language
-            kwarg.
+        intent (string): the intent, returned from wit.ai used to determine the correct func
     """
-    Ariane().register(intent, func)
+    def _inner_register(func):
+        """Inner decorator for registering a feature.
+
+        Args:
+            func (callable): a "feature" of ariane. Musst accept a ariane instance as first
+            argument and a wid response as second.
+        """
+        Ariane.register(intent, func)
+        return func
+    return _inner_register
 
 
 def register_js(js_file, template_id=None):
@@ -40,4 +70,4 @@ def register_js(js_file, template_id=None):
         template_id (string): The id used for the script tag in html. If provided, the type will
         be set to text/html.
     """
-    Ariane().js_files.append((js_file, template_id))
+    Ariane.js_files.append((js_file, template_id))
