246ba0a8d11a37dade759b9b8e94e5bf6a5d3d0f
Max Brauer <max@transcode.de>
2016-11-06 20:52:16 +0100

    tests: Added basic docstrings.

--- tests/wiki/conftest.py
+++ tests/wiki/conftest.py
@@ -3,9 +3,11 @@ import pytest
 
 @pytest.fixture
 def wiki_response():
+    """Example responsetext for the wikipedia package."""
     return "Lorem Ipsum dolor sit amet..."
 
 
 @pytest.fixture
 def wit_resp(wiki_response):
+    """WitResponse to pass into get_wiki_article."""
     return {'entities': {'wikipedia_search_query': [{'value': wiki_response}]}}
