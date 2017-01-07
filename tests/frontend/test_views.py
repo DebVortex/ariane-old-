bf022fff9a7468293112aa05ea72426a49b7f5eb
Max Brauer <max@transcode.de>
2017-01-07 15:46:57 +0100

    frontend: removed no longer needed test

--- tests/frontend/test_views.py
+++ tests/frontend/test_views.py
@@ -2,21 +2,6 @@ import pytest
 from django.core.urlresolvers import reverse
 
 
-class TestLandingPageView(object):
-    """Tests for the LandingPageView."""
-
-    def test_unauthorized(self, client):
-        """Test that accessing the LandingPageView unauthorized resturns 200."""
-        response = client.get(reverse('index'))
-        assert response.status_code == 200
-
-    @pytest.mark.django_db
-    def test_authorized(self, login, client):
-        """Test that accessing as a logged in user the LandingPageView causes an redirect."""
-        response = client.get(reverse('index'))
-        assert response.status_code == 302
-
-
 class TestArianeView(object):
     """Tests for the ArianeView."""
 
