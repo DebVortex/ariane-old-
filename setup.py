fe4307a9f05d0cc0720d68ce15a9c73d1ffa289e
requires.io <support@requires.io>
2017-06-16 04:39:45 +0200

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ extras_require = {}
 requires = [
     'asgiref==1.1.2',
     'channels==1.1.4',
-    'daphne==1.2.0',
+    'daphne==1.3.0',
     'Django==1.11.2',
     'dj-database-url==0.4.2',
     'django-allauth==0.32.0',
